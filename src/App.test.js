78f5a922617e9c95eba8896bd64f8edde05094fd
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2023-04-24 22:39:22 +0200

    Copie du code de App.js (mal placé dans utils et renommé en typography.js) dans Home.jsx

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 // import App from './App';
-import Home from "./Home"
+import Home from "./Home";
 
 test('renders learn react link', () => {
   // render(<App />);
