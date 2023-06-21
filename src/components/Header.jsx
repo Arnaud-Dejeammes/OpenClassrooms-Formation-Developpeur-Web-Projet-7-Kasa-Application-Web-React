b5c3c76828cf0021afc0fe5844f9c39dbe282c17
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2023-06-21 14:21:27 +0200

    Initialisation de styled-elements et utilisation d'un composant fonctionnel pour la police des liens du header avec logique couleur

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components"
+import colors from "../styles/colors"
+
+const StyledLink = styled(Link)`    
+    font-size: 24px;
+    font-weight: 500;
+    line-height: 34px;
+    letter-spacing: 0em;
+    text-align: right;
+    color: ${(props) => (props.isPrimary ? colors.primary : colors.secondary)};
+`
 
 function Header() {
     return (
         <nav>
-            <Link to="/">ACCUEIL</Link>
-            <Link to="/about">A PROPOS</Link>
+            <StyledLink to="/" isPrimary>ACCUEIL</StyledLink>
+            <StyledLink to="/about" isPrimary>A PROPOS</StyledLink>
         </nav>
     )
 }
